c88f38e1fddc7e9cc81190f591fa10a37f5e3d5d
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-10-02 17:57:29 -0700

    added: all mandatory files

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,7 +4,7 @@
  * check_cycle - checks if a singly linked list has a cycle in it
  * @list: pointer to the head of the list
  * Return: 0 if there is no cycle, 1 if otherwise
- *     */
+ */
 int check_cycle(listint_t *list)
 {
 	listint_t *slow = list
