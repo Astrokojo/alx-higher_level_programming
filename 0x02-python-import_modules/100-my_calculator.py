fbe59764055854079c6eca3bf7fce0a523780486
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-10-05 18:06:42 +0000

    added: pyccodestyle complaint

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python:3
+#!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
+        
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
