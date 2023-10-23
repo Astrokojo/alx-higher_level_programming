f67a0fef74f458e8bd8a8dcdeaf2787912e7092d
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-10-23 15:42:08 +0000

    s

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end='')
             counter += 1
     except IndexError:
-        print("the list index you input is likely out of range ")
         pass
     finally:
         print()
