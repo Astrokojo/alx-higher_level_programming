bfc6e33b550fd10dc1951d7c58f167042e1b8594
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-10-24 21:30:42 +0000

    s

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-class Square:
+"""A class called Square"""
 
+
+class Square:
+    """Represents Square"""
     def __init__(self, size=0):
+        """Initializes the square"""
         if type(size) is int:
             if size < 0:
                 raise ValueError('size must be >= 0')
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Definition"""
+
+
 class Square:
+    """Representation"""
     def __init__(self, size=0):
         if type(size) is int:
+            """Initiialization"""
             if size < 0:
                 raise ValueError('size must be >= 0')
             else:
@@ -10,4 +15,5 @@ class Square:
                 raise TypeError('size must be an integer')
 
     def area(self):
+        """CAlculate and return the area of square"""
         return self.__size ** 2
