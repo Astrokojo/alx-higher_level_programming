90ece57364e9144e79e8b9432adb91b90b879252
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-10-31 21:21:12 +0000

    dgfdg

---
+++ 0x08-python-more_classes/main/2-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+class Rectangle:
+    """ a rectangle class """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    def width(self):
+        return self.__width
+
+    def width(self, value):
+        if type(value) is int:
+            self.__width = value
+        else:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+
+    def height(self):
+        return self.__height
+
+    def height(self, value):
+        if type(value) is int:
+            self.__height = value
+        else:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
