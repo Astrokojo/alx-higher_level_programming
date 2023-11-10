7db06cce3fa401fa2f0167729993f4cec11da44a
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-11-10 21:20:04 +0000

    hjbkj

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,8 +23,10 @@ class Rectangle(Base):
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
+        
         if value <= 0:
             raise ValueError("width must be > 0")
+        
         self.__width = value
     
     @property   
@@ -35,8 +37,10 @@ class Rectangle(Base):
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
+        
         if value <= 0:
             raise ValueError("height must be > 0")
+        
         self.__height = value
     
     @property
@@ -47,8 +51,10 @@ class Rectangle(Base):
     def x(self, value):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
+        
         if value < 0:
             raise ValueError("x must be >= 0")
+        
         self.__x = value
     
     @property
@@ -59,9 +65,24 @@ class Rectangle(Base):
     def y(self, value):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
+        
         if value < 0:
             raise ValueError("y must be >= 0")
+        
         self.__y = value
         
     def area(self):
         return self.__width * self.__height
+    
+    def display(self):
+        for i in range(self.__y):
+            print()
+            
+        for i in range(self.__height):
+            print("#" * self.__width)
+            
+    def __str__(self) -> str:
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+    
+    def update(self, *args):
+        
