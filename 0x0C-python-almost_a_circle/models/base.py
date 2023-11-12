06a33f1e3ae24438f67b6910d50f2c69c16eaa15
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-11-12 20:27:37 +0000

    xc

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,7 @@
 """
 Module has the first class Base
 """
-
+import json
 
 class Base():
     __nb_objects = 0
@@ -12,3 +12,14 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == [] :
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+        
+    def save_to_file(cls, list_objs):
+        with open(cls, 'w', encoding="utf-8") as file:
+            json.dump(list_objs, file)
