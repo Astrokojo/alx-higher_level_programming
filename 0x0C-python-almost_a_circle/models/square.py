d865d483e269953bddf03e434bb78f8b0eb82b5c
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-11-13 23:23:23 +0000

    pycodestyle?

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,7 +29,6 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-        self.height = value
 
     def update(self, *args, **kwargs):
         if args:
