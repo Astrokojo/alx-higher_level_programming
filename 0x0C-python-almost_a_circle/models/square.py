714889bd7c37ad71658ed75c6fa8670b9ca2c0fe
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-11-13 19:57:14 +0000

    gd

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,35 +5,34 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-    
+
     @property
     def size(self):
-        return self.width 
-    
+        return self.width
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        
+
         if value <= 0:
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
-           
+
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-    
+
     def area(self):
         return self.__width * self.__height
-    
+
     def display(self):
         for i in range(self.__y):
             print()
-            
+
         for i in range(self.__height):
             print("#" * self.__width)
-            
-    
+
     def update(self, *args, **kwargs):
         if args:
             if len(args) >= 1:
@@ -44,11 +43,11 @@ class Square(Rectangle):
                 self.x = args[2]
             if len(args) >= 4:
                 self.y = args[3]
-        
+
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
-                
+
     def to_dictionary(self):
         """Return the dictionary representation of the Square."""
         return {
