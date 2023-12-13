28838f9a13adb1379b382ccfaa5759b644953fdf
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-12-13 15:58:37 +0000

    added task 3

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- Create database: hbtn_0d_2
 -- create user: user_0d_2
+-- grant privilege: SELECT to user: 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT on hbtn_0d_2.* TO 'user_0d_2'@'localhost';
