c9f5abd6693026b5b49fc056e2bb50d43ded44dc
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-01-10 06:56:40 +0000

    asa

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 const square = require('./5-square');
 class Square extends square {
-	constructor (size) {
-		super(size);
-	}
-	charPrint (c) {
-		if (c === undefined) {
-			c = 'X';
-		} else {
-			for (let step = 0; step < this.height; step++) { console.log(c.repeat(this.width));
-			}
-		}
-	}
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let step = 0; step < this.height; step++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
 }
 module.exports = Square;
